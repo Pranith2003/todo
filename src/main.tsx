1ba38e1c89d61fd1ced38ebb58273ec2b327adb7
Pranith <Pranithtettabavi@gmail.com>
2025-01-02 01:16:54 +0530

    added: real score form vercel

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "../redux/store.ts";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
+      <SpeedInsights />
       <App />
     </Provider>
   </StrictMode>

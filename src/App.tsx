6b6dd873fd44613516ca0a5c01cd44708186b3c2
Pranith <Pranithtettabavi@gmail.com>
2025-01-02 12:35:11 +0530

    feat: Dark & light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import InputField from "./components/custom/InputField";
+import { ThemeProvider } from "./components/custom/ThemeProvider";
 import TodoDisplay from "./components/custom/TodoDisplay";
+import { ModeToggle } from "./components/custom/Toggle";
 import { Toaster } from "./components/ui/toaster";
 function App() {
   return (
     <>
-    <Toaster />
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <div className="flex justify-center pt-[5px]">
+          <ModeToggle />
+        </div>
+      </ThemeProvider>
+      <Toaster />
       <div className="sm:w-[40vw] h-screen mx-auto p-10">
         <InputField />
         <TodoDisplay />
